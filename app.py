84f0ab1ce7df03b7f9f1d0c6744a2cf10421452c
Mohammed Zaid <149226252+MohammedZaid-AI@users.noreply.github.com>
2025-10-13 12:24:06 +0530

    updated

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pprint
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from mcp_use import MCPClient,MCPAgent
-
+from random import choice
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -34,22 +34,24 @@ async def main():
         system_prompt=SYSTEM_PROMPT
         )
 
-    # reddit_response =await agent.run("Fetch 5 recent posts from r/SaaS. For each post, include only the top 10 comments that describe problems, struggles, or challenges (e.g., issues with pricing, scaling, growth, customer retention, marketing, or technical difficulties). Ignore generic, positive, or promotional comments. Return only the problem-focused comments along with the post title and URL")
-    # pprint.pprint(reddit_response)
+    user_query=input(choice(user_display_list))
 
-    # tweets = await agent.run("get_tweets('#buildinpublic', 10)")
-    # pprint.pprint(tweets)
+    reddit_response =await agent.run(user_query)
+    pprint.pprint(reddit_response)
+
+    tweets = await agent.run(user_query)
+    pprint.pprint(tweets)
    
-    # youtube_response = await agent.run("fetch_youtube_videos('krish naik vids about langchain')")
-    # pprint.pprint(youtube_response)
+    youtube_response = await agent.run(user_query)
+    pprint.pprint(youtube_response)
     
-    # wiki_response = await agent.run("search('OpenAI')")
-    # pprint.pprint(wiki_response)   
+    wiki_response = await agent.run(user_query)
+    pprint.pprint(wiki_response)   
 
-    google_results = await agent.run("Search latest AI funding rounds in 2025 using Google Search MCP.")
+    google_results = await agent.run(user_query)
     pprint.pprint(google_results)
     
-    medium_results = await agent.run("Search latest articles on AI in 2025 using Medium Search MCP.")
+    medium_results = await agent.run(user_query)
     pprint.pprint(medium_results)
 
 if __name__ == "__main__":
